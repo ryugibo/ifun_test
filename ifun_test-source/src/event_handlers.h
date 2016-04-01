f522e35d9b2edd1ebe347c4635494aa3f1d38cbb
Gibo Ryu <ryugibo@daum.net>
2016-04-01 02:03:12 -0700

    Upload a blank project
    Signed-off-by: Gibo Ryu <ryugibo@daum.net>

---
+++ ifun_test-source/src/event_handlers.h
@@ -0,0 +1,20 @@
+// PLEASE ADD YOUR EVENT HANDLER DECLARATIONS HERE.
+
+#ifndef SRC_EVENT_HANDLERS_H_
+#define SRC_EVENT_HANDLERS_H_
+
+#include <funapi.h>
+
+#include "ifun_test_messages.pb.h"
+#include "ifun_test_object.h"
+#include "ifun_test_rpc_messages.pb.h"
+#include "ifun_test_types.h"
+
+
+namespace ifun_test {
+
+void RegisterEventHandlers();
+
+}  // namespace ifun_test
+
+#endif  // SRC_EVENT_HANDLERS_H_
---
+++ ifun_test-source/src/ifun_test_types.h
@@ -0,0 +1,16 @@
+#ifndef SRC_IFUN_TEST_TYPES_H_
+#define SRC_IFUN_TEST_TYPES_H_
+
+#include <funapi.h>
+
+
+namespace ifun_test {
+
+using fun::string;
+using fun::shared_ptr;
+using fun::weak_ptr;
+using fun::Uuid;
+
+}  // namespace ifun_test
+
+#endif  // SRC_IFUN_TEST_TYPES_H_
